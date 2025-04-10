015892e57684e9c829cb37c68e18fda53abd62f8
Ankush-accio <ankushrohilla2304@gmail.com>
2025-04-10 13:20:16 +0530

    Update Landing.jsx

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -22,7 +22,7 @@ const Landing = () => {
 
 
             <div className='flex text-white items-center gap-6'>
-                <p>Swiggy Corporate</p>
+                <p>Cravish Corporate</p>
                 <p>Partner with us</p>
                 <button className='text-white border px-8 py-4 rounded-lg'>
                     Get the App<i className="fa-solid fa-arrow-right fa-rotate-by mx-2" >
@@ -36,7 +36,7 @@ const Landing = () => {
         <div className='flex relative'>
             <img className='absolute left-0 h-[450px] w-[250px]' src={veggie} alt="" />
             <div className=' h-[100%]  w-[70%] mx-auto items-center gap-10 flex flex-col'>
-                <h1 className='text-[48px] text-white text-center'>Order food & groceries. Discover <br/> best restaurants. Swiggy it!</h1>
+                <h1 className='text-[48px] text-white text-center'>Order food & groceries. Discover <br/> best restaurants. Cravish it!</h1>
                 <div className='w-[50%] relative' >
                     <input className='w-[100%] h-[56px] rounded-lg p-4' type="text"  placeholder='Search for restaurant, item or more'/>
                     <i class="fa-solid fa-magnifying-glass text-black absolute top-5 right-5" ></i>
